f897c3fd327343c5ba7880421e24616682fc24f9
Renkel <guirenkel@gmail.com>
2023-06-13 11:25:19 -0300

    fix: fix web-to-nginx server port

--- k8s-web-to-nginx/index.mjs
+++ k8s-web-to-nginx/index.mjs
@@ -3,7 +3,7 @@ import fetch from 'node-fetch'
 import os from 'os'
 
 const app = express()
-const PORT = 1111
+const PORT = 3000
 
 app.get("/", (req, res) => {
   const helloMessage = `Hello from the ${os.hostname()}`
